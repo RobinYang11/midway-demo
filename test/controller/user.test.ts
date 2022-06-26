13f69e1cbd5a0f7bb33fff7498e8c17d03d14f5b
robin_yang <yangguoping9543@dingtalk.com>
2022-06-26 22:07:40 +0800

    feat:添加用户登录测试用例

--- test/controller/user.test.ts
+++ test/controller/user.test.ts
@@ -1,6 +1,7 @@
 
 import { createApp, close, createHttpRequest } from '@midwayjs/mock';
 import { Framework } from '@midwayjs/koa';
+import * as assert from 'assert';
 
 describe('test/controller/home.user.ts', () => {
 
@@ -8,17 +9,29 @@ describe('test/controller/home.user.ts', () => {
     // create app
     const app = await createApp<Framework>();
 
-    // make request
-    const result = await createHttpRequest(app)
-    .post("/api/user/login")
-    .send({
-      username:"jack",
-      password:"redballoon"
-    })
 
-    // use expect by jest
+    // 添加用户测试用例
+    const addUserResult = await createHttpRequest(app)
+      .post("/api/user/add")
+      .send({
+        username: "jack",
+        password: "redballoon"
+      })
+
+    expect(addUserResult.status).toBe(200);
+
+    //用户登录测试用例
+    const result = await createHttpRequest(app)
+      .post("/api/user/login")
+      .send({
+        username: "jack",
+        password: "redballoon"
+      })
+
     expect(result.status).toBe(200);
-    // close app
+    assert.deepStrictEqual(result.body.code, 200)
+    assert.deepStrictEqual(result.body.result, "登录成功")
+
     await close(app);
   });
 
